9887fbda267be6be651f873992c7d267de6c4959
Danpeo <danilvar4@gmail.com>
2024-01-19 23:50:44 +0400

    Add project files.

---
+++ Danilvar.UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Danilvar.UnitOfWork;
+
+public class UnitOfWork<T> where T : DbContext
+{
+    private readonly T _context;
+
+    public UnitOfWork(T context)
+    {
+        _context = context;
+    }
+
+    public async Task<bool> CompleteAsync() =>
+        await _context.SaveChangesAsync() > 0;
+
+    public bool HasChanges() =>
+        _context.ChangeTracker.HasChanges();
+}
